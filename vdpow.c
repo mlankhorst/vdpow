2ef328c8dbf5be00a5346a36c178e7080901f5a3
Maarten Lankhorst <m.b.lankhorst@gmail.com>
2011-10-12 18:50:41 +0200

    create calls, and move function pointers

--- vdpow.c
+++ vdpow.c
@@ -1,90 +1,48 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <X11/Xlib.h>
 #include <vdpau/vdpau.h>
 #include <vdpau/vdpau_x11.h>
+#include "vdpow.h"
+
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(*a))
 
 static const int output_width = 64;
 static const int output_height = 64;
+static const int input_width = 64;
+static const int input_height = 64;
 
 VdpDevice dev;
-VdpGetProcAddress * vdp_get_proc_address;
-
-VdpGetErrorString * vdp_get_error_string;
-VdpGetApiVersion * vdp_get_api_version;
-VdpGetInformationString * vdp_get_information_string;
-VdpDeviceDestroy * vdp_device_destroy;
-VdpGenerateCSCMatrix * vdp_generate_csc_matrix;
-VdpVideoSurfaceQueryCapabilities * vdp_video_surface_query_capabilities;
-VdpVideoSurfaceQueryGetPutBitsYCbCrCapabilities * vdp_video_surface_query_get_put_bits_y_cb_cr_capabilities;
-VdpVideoSurfaceCreate * vdp_video_surface_create;
-VdpVideoSurfaceDestroy * vdp_video_surface_destroy;
-VdpVideoSurfaceGetParameters * vdp_video_surface_get_parameters;
-VdpVideoSurfaceGetBitsYCbCr * vdp_video_surface_get_bits_y_cb_cr;
-VdpVideoSurfacePutBitsYCbCr * vdp_video_surface_put_bits_y_cb_cr;
-VdpOutputSurfaceQueryCapabilities * vdp_output_surface_query_capabilities;
-VdpOutputSurfaceQueryGetPutBitsNativeCapabilities * vdp_output_surface_query_get_put_bits_native_capabilities;
-VdpOutputSurfaceQueryPutBitsIndexedCapabilities * vdp_output_surface_query_put_bits_indexed_capabilities;
-VdpOutputSurfaceQueryPutBitsYCbCrCapabilities * vdp_output_surface_query_put_bits_y_cb_cr_capabilities;
-VdpOutputSurfaceCreate * vdp_output_surface_create;
-VdpOutputSurfaceDestroy * vdp_output_surface_destroy;
-VdpOutputSurfaceGetParameters * vdp_output_surface_get_parameters;
-VdpOutputSurfaceGetBitsNative * vdp_output_surface_get_bits_native;
-VdpOutputSurfacePutBitsNative * vdp_output_surface_put_bits_native;
-VdpOutputSurfacePutBitsIndexed * vdp_output_surface_put_bits_indexed;
-VdpOutputSurfacePutBitsYCbCr * vdp_output_surface_put_bits_y_cb_cr;
-VdpBitmapSurfaceQueryCapabilities * vdp_bitmap_surface_query_capabilities;
-VdpBitmapSurfaceCreate * vdp_bitmap_surface_create;
-VdpBitmapSurfaceDestroy * vdp_bitmap_surface_destroy;
-VdpBitmapSurfaceGetParameters * vdp_bitmap_surface_get_parameters;
-VdpBitmapSurfacePutBitsNative * vdp_bitmap_surface_put_bits_native;
-VdpOutputSurfaceRenderOutputSurface * vdp_output_surface_render_output_surface;
-VdpOutputSurfaceRenderBitmapSurface * vdp_output_surface_render_bitmap_surface;
-VdpDecoderQueryCapabilities * vdp_decoder_query_capabilities;
-VdpDecoderCreate * vdp_decoder_create;
-VdpDecoderDestroy * vdp_decoder_destroy;
-VdpDecoderGetParameters * vdp_decoder_get_parameters;
-VdpDecoderRender * vdp_decoder_render;
-VdpVideoMixerQueryFeatureSupport * vdp_video_mixer_query_feature_support;
-VdpVideoMixerQueryParameterSupport * vdp_video_mixer_query_parameter_support;
-VdpVideoMixerQueryAttributeSupport * vdp_video_mixer_query_attribute_support;
-VdpVideoMixerQueryParameterValueRange * vdp_video_mixer_query_parameter_value_range;
-VdpVideoMixerQueryAttributeValueRange * vdp_video_mixer_query_attribute_value_range;
-VdpVideoMixerCreate * vdp_video_mixer_create;
-VdpVideoMixerSetFeatureEnables * vdp_video_mixer_set_feature_enables;
-VdpVideoMixerSetAttributeValues * vdp_video_mixer_set_attribute_values;
-VdpVideoMixerGetFeatureSupport * vdp_video_mixer_get_feature_support;
-VdpVideoMixerGetFeatureEnables * vdp_video_mixer_get_feature_enables;
-VdpVideoMixerGetParameterValues * vdp_video_mixer_get_parameter_values;
-VdpVideoMixerGetAttributeValues * vdp_video_mixer_get_attribute_values;
-VdpVideoMixerDestroy * vdp_video_mixer_destroy;
-VdpVideoMixerRender * vdp_video_mixer_render;
-VdpPresentationQueueTargetDestroy * vdp_presentation_queue_target_destroy;
-VdpPresentationQueueCreate * vdp_presentation_queue_create;
-VdpPresentationQueueDestroy * vdp_presentation_queue_destroy;
-VdpPresentationQueueSetBackgroundColor * vdp_presentation_queue_set_background_color;
-VdpPresentationQueueGetBackgroundColor * vdp_presentation_queue_get_background_color;
-VdpPresentationQueueGetTime * vdp_presentation_queue_get_time;
-VdpPresentationQueueDisplay * vdp_presentation_queue_display;
-VdpPresentationQueueBlockUntilSurfaceIdle * vdp_presentation_queue_block_until_surface_idle;
-VdpPresentationQueueQuerySurfaceStatus * vdp_presentation_queue_query_surface_status;
-VdpPreemptionCallbackRegister * vdp_preemption_callback_register;
-VdpPresentationQueueTargetCreateX11 * vdp_presentation_queue_target_create_x11;
-
 
 int main(int argc, char *argv[]) {
    Display *display;
    Window root, window;
-   VdpGetProcAddress *vdp_dlsym;
    VdpStatus ret;
-   VdpPresentationQueueTargetCreateX11 *vdp_presentation_queue_target_create_x11;
    VdpPresentationQueueTarget target;
-   
+   VdpDecoder dec;
+   VdpVideoSurface surf[8];
+   VdpOutputSurface osurf[8];
+   VdpVideoMixer mixer;
+   int i;
+
+   VdpVideoMixerFeature mixer_features[] = {
+      VDP_VIDEO_MIXER_FEATURE_SHARPNESS,
+      VDP_VIDEO_MIXER_FEATURE_HIGH_QUALITY_SCALING_L9
+   };
+   VdpVideoMixerParameter mixer_parameters[] = {
+      VDP_VIDEO_MIXER_PARAMETER_VIDEO_SURFACE_WIDTH,
+      VDP_VIDEO_MIXER_PARAMETER_VIDEO_SURFACE_HEIGHT
+   };
+   const void *mixer_values[ARRAY_SIZE(mixer_parameters)] = {
+      &input_width,
+      &input_height
+   };
+
    display = XOpenDisplay(NULL);
    root = XDefaultRootWindow(display);
    window = XCreateSimpleWindow(display, root, 0, 0, output_width, output_height, 0, 0, 0);
-
    XSelectInput(display, window, ExposureMask | KeyPressMask);
    XMapWindow(display, window);
    XSync(display, 0);
@@ -95,71 +53,18 @@ int main(int argc, char *argv[]) {
    ret = vdp_device_create_x11(display, 0, &dev, &vdp_get_proc_address);
    assert(ret == VDP_STATUS_OK);
 
-#define GET_POINTER(fid, fn) ret = vdp_get_proc_address(dev, fid, (void**)&fn); assert(ret == VDP_STATUS_OK);
-   GET_POINTER(VDP_FUNC_ID_PREEMPTION_CALLBACK_REGISTER,                          vdp_preemption_callback_register);
-   GET_POINTER(VDP_FUNC_ID_GET_ERROR_STRING,                                      vdp_get_error_string);
-   GET_POINTER(VDP_FUNC_ID_GET_API_VERSION,                                       vdp_get_api_version);
-   GET_POINTER(VDP_FUNC_ID_GET_INFORMATION_STRING,                                vdp_get_information_string);
-   GET_POINTER(VDP_FUNC_ID_DEVICE_DESTROY,                                        vdp_device_destroy);
-   GET_POINTER(VDP_FUNC_ID_GENERATE_CSC_MATRIX,                                   vdp_generate_csc_matrix);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_QUERY_CAPABILITIES,                      vdp_video_surface_query_capabilities);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_QUERY_GET_PUT_BITS_Y_CB_CR_CAPABILITIES, vdp_video_surface_query_get_put_bits_y_cb_cr_capabilities);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_CREATE,                                  vdp_video_surface_create);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_DESTROY,                                 vdp_video_surface_destroy);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_GET_PARAMETERS,                          vdp_video_surface_get_parameters);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_GET_BITS_Y_CB_CR,                        vdp_video_surface_get_bits_y_cb_cr);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_SURFACE_PUT_BITS_Y_CB_CR,                        vdp_video_surface_put_bits_y_cb_cr);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_QUERY_CAPABILITIES,                     vdp_output_surface_query_capabilities);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_QUERY_GET_PUT_BITS_NATIVE_CAPABILITIES, vdp_output_surface_query_get_put_bits_native_capabilities);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_QUERY_PUT_BITS_INDEXED_CAPABILITIES,    vdp_output_surface_query_put_bits_indexed_capabilities);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_QUERY_PUT_BITS_Y_CB_CR_CAPABILITIES,    vdp_output_surface_query_put_bits_y_cb_cr_capabilities);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_CREATE,                                 vdp_output_surface_create);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_DESTROY,                                vdp_output_surface_destroy);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_GET_PARAMETERS,                         vdp_output_surface_get_parameters);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_GET_BITS_NATIVE,                        vdp_output_surface_get_bits_native);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_PUT_BITS_NATIVE,                        vdp_output_surface_put_bits_native);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_PUT_BITS_INDEXED,                       vdp_output_surface_put_bits_indexed);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_PUT_BITS_Y_CB_CR,                       vdp_output_surface_put_bits_y_cb_cr);
-   GET_POINTER(VDP_FUNC_ID_BITMAP_SURFACE_QUERY_CAPABILITIES,                     vdp_bitmap_surface_query_capabilities);
-   GET_POINTER(VDP_FUNC_ID_BITMAP_SURFACE_CREATE,                                 vdp_bitmap_surface_create);
-   GET_POINTER(VDP_FUNC_ID_BITMAP_SURFACE_DESTROY,                                vdp_bitmap_surface_destroy);
-   GET_POINTER(VDP_FUNC_ID_BITMAP_SURFACE_GET_PARAMETERS,                         vdp_bitmap_surface_get_parameters);
-   GET_POINTER(VDP_FUNC_ID_BITMAP_SURFACE_PUT_BITS_NATIVE,                        vdp_bitmap_surface_put_bits_native);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_RENDER_OUTPUT_SURFACE,                  vdp_output_surface_render_output_surface);
-   GET_POINTER(VDP_FUNC_ID_OUTPUT_SURFACE_RENDER_BITMAP_SURFACE,                  vdp_output_surface_render_bitmap_surface);
-   GET_POINTER(VDP_FUNC_ID_DECODER_QUERY_CAPABILITIES,                            vdp_decoder_query_capabilities);
-   GET_POINTER(VDP_FUNC_ID_DECODER_CREATE,                                        vdp_decoder_create);
-   GET_POINTER(VDP_FUNC_ID_DECODER_DESTROY,                                       vdp_decoder_destroy);
-   GET_POINTER(VDP_FUNC_ID_DECODER_GET_PARAMETERS,                                vdp_decoder_get_parameters);
-   GET_POINTER(VDP_FUNC_ID_DECODER_RENDER,                                        vdp_decoder_render);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_QUERY_FEATURE_SUPPORT,                     vdp_video_mixer_query_feature_support);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_QUERY_PARAMETER_SUPPORT,                   vdp_video_mixer_query_parameter_support);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_QUERY_ATTRIBUTE_SUPPORT,                   vdp_video_mixer_query_attribute_support);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_QUERY_PARAMETER_VALUE_RANGE,               vdp_video_mixer_query_parameter_value_range);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_QUERY_ATTRIBUTE_VALUE_RANGE,               vdp_video_mixer_query_attribute_value_range);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_CREATE,                                    vdp_video_mixer_create);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_SET_FEATURE_ENABLES,                       vdp_video_mixer_set_feature_enables);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_SET_ATTRIBUTE_VALUES,                      vdp_video_mixer_set_attribute_values);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_GET_FEATURE_SUPPORT,                       vdp_video_mixer_get_feature_support);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_GET_FEATURE_ENABLES,                       vdp_video_mixer_get_feature_enables);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_GET_PARAMETER_VALUES,                      vdp_video_mixer_get_parameter_values);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_GET_ATTRIBUTE_VALUES,                      vdp_video_mixer_get_attribute_values);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_DESTROY,                                   vdp_video_mixer_destroy);
-   GET_POINTER(VDP_FUNC_ID_VIDEO_MIXER_RENDER,                                    vdp_video_mixer_render);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_TARGET_DESTROY,                     vdp_presentation_queue_target_destroy);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_CREATE,                             vdp_presentation_queue_create);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_DESTROY,                            vdp_presentation_queue_destroy);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_SET_BACKGROUND_COLOR,               vdp_presentation_queue_set_background_color);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_GET_BACKGROUND_COLOR,               vdp_presentation_queue_get_background_color);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_GET_TIME,                           vdp_presentation_queue_get_time);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_DISPLAY,                            vdp_presentation_queue_display);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_BLOCK_UNTIL_SURFACE_IDLE,           vdp_presentation_queue_block_until_surface_idle);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_QUERY_SURFACE_STATUS,               vdp_presentation_queue_query_surface_status);
-   GET_POINTER(VDP_FUNC_ID_PREEMPTION_CALLBACK_REGISTER,                          vdp_preemption_callback_register);
-   GET_POINTER(VDP_FUNC_ID_PRESENTATION_QUEUE_TARGET_CREATE_X11,                  vdp_presentation_queue_target_create_x11);
+   load_vdpau(dev);
 
-   ret = vdp_presentation_queue_target_create_x11(dev, window, &target);
-   assert(ret == VDP_STATUS_OK);
+#define ok(a) do { ret = a; if (ret != VDP_STATUS_OK) { fprintf(stderr, "%s\n", vdp_get_error_string(ret)); return 1; } } while (0)
+
+   ok(vdp_presentation_queue_target_create_x11(dev, window, &target));
+   for (i = 0; i < ARRAY_SIZE(surf); ++i) {
+      ok(vdp_video_surface_create(dev, VDP_CHROMA_TYPE_420, input_width, input_height, &surf[i]));
+      ok(vdp_output_surface_create(dev, VDP_COLOR_TABLE_FORMAT_B8G8R8X8, output_width, output_height, &osurf[i]));
+   }
+   ok(vdp_video_mixer_create(dev, ARRAY_SIZE(mixer_features), mixer_features,
+                             ARRAY_SIZE(mixer_parameters), mixer_parameters, mixer_values, &mixer));
+   ok(vdp_decoder_create(dev, VDP_DECODER_PROFILE_MPEG1, input_width, input_height, 2, &dec));
 
    while (1) {
       XEvent ev;
@@ -171,6 +76,14 @@ int main(int argc, char *argv[]) {
    }
 
 out:
+   vdp_decoder_destroy(dec);
+   vdp_video_mixer_destroy(mixer);
+   for (i = ARRAY_SIZE(surf); i > 0; i--) {
+      vdp_output_surface_destroy(osurf[i - 1]);
+      vdp_video_surface_destroy(surf[i - 1]);
+   }
+   vdp_presentation_queue_target_destroy(target);
+   vdp_device_destroy(dev);
    XDestroyWindow(display, window);
    XCloseDisplay(display);
    return 0;
